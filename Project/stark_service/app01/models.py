0306a672bd721509efeee13fc5c8978a3e400540
taoL2018 <tao_l_2018@163.com>
2019-08-12 23:43:53 +0800

    update stark_service

--- Project/stark_service/app01/models.py
+++ Project/stark_service/app01/models.py
@@ -6,3 +6,7 @@ from django.db import models
 class Role(models.Model):
 
     name = models.CharField(max_length=32, verbose_name="角色名", unique=True)
+
+    def __str__(self):
+
+        return self.name
--- Project/stark_service/app02/models.py
+++ Project/stark_service/app02/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from app01.models import Role
 
 # Create your models here
 
@@ -6,3 +7,7 @@ from django.db import models
 class User(models.Model):
 
     name = models.CharField(max_length=32, verbose_name="用户名", unique=True)
+
+    def __str__(self):
+
+        return self.name
