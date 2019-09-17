4e083bba3eb47a383f713966e4f0dab6ff8a2b5c
taoL2018 <tao_l_2018@163.com>
2019-09-17 20:36:44 +0800

    update sort

--- DataStructure/Simple_algorithm_demo/bubble_sort.py
+++ DataStructure/Simple_algorithm_demo/bubble_sort.py
@@ -13,6 +13,29 @@ def bubble_sort(list):
                 list[j+1], list[j] = list[j], list[j+1]
         print("执行第%s次：%s" % (i, list))
 
+def bubble_sort_2(alist):
+    """
+        冒泡排序
+        原理: 遍历元素, 对比当前元素和后一个元素, 并交换元素位置, 来达到排序目的;
+    :param alist: 排序的列表
+    :return: 排好序的列表
+    """
+    length = len(alist)
+    if length <= 1:
+
+        return alist
+    j = 0
+    while j < length-1:
+        i = 0
+        while i < length-1-j:  # 这里减掉j可以减少遍历次数
+            if alist[i] > alist[i+1]:
+                alist[i], alist[i+1] = alist[i+1], alist[i]
+            i += 1
+        j += 1
+        print(i)
+
+    return alist
+
 
 if __name__ == '__main__':
     list = [5, 2, 3, 4, 8, 2, 7]
